60300f968721f2bf857d2e82a4e12ee1ecc44f58
Roque Costa <devroque18@gmail.com>
2022-10-03 20:51:16 -0300

    Adding fix client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <h1 className="text-3x1 font-bold underline text-red-600">Simple React</h1>
+  );
+}
+
+export default App;
